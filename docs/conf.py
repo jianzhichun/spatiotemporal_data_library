ddbedb5f6de8f9ef24fc25ba03e2fb3467dca737
chun <zzchun12826@gmail.com>
2025-05-23 08:13:04 +0800

    update

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../spatiotemporal_data_library'))
 
 project = 'spatiotemporal_data_library'
 copyright = '2024 Zao Zhang'
